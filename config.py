cd49a8e3e066eedd4475011c410a268024e0c76d
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-28 16:26:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,8 +54,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/veron_bots")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/privatevoicechatgroup")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NexaCoders")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NexaMeetup")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", None))
