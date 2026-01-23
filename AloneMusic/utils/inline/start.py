a031b0865cab1d4a3c64cadd407bcd2c943d6c20
sexyxcoders <233089432+sexyxcoders@users.noreply.github.com>
2026-01-23 14:54:44 +0000

    Auto update: full repo auto-fix

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -33,11 +33,9 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-         InlineKeyboardButton(
-                text="💌 ʏᴛ-ᴀᴘɪ",
-                callback_data="bot_info_data"
-            ),
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="💌 ʏᴛ-ᴀᴘɪ", callback_data="bot_info_data"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
